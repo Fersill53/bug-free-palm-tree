ce2dfe3b81b611edbbec0a6cbbeb0184a9ef7840
Fernando Siller <fernando@Fernandos-MBP.attlocal.net>
2023-04-21 20:29:20 -0500

    added time slots to html, const to js

--- Assets/script.js
+++ Assets/script.js
@@ -1,7 +1,7 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+//$(function () {
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
     // local storage. HINT: What does `this` reference in the click listener
@@ -20,5 +20,26 @@ $(function () {
     // attribute of each time-block be used to do this?
     //
     // TODO: Add code to display the current date in the header of the page.
-  });
-  
+
+
+const curDay = document.getElementById('currentDay');
+const saveBtn = document.getElementById('save')
+const todDat = document.getElementById('currentDay')
+const eight = document.getElementById('hour-8')
+const nine = document.getElementById('hour-9')
+const eleven = document.getElementById('hour-11')
+const tweleve = document.getElementById('hour-12')
+const one = document.getElementById('hour-13')
+const two = document.getElementById('hour-14')
+const three = document.getElementById('hour-15')
+const four = document.getElementById('hour-16')
+const five = document.getElementById('hour-17')
+
+const today = dayjs();
+    $('currentDay').text(today.format('MMM D, YYYY'));
+
+
+//saveBtn.addEventListener('click', function(event)) {
+   // localStorage.setItem("save")
+//}
+    
